8c6ffdf7bab02ea3f80eb457fc0b72067836ff97
ajay26188 <ajaysah52@gmail.com>
2024-01-16 00:07:00 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import MyContainer from './components/MyContainer';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <MyContainer />
     </div>
   );
 }
---
+++ src/components/MyContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyList from './MyList'
+
+const MyContainer = () => {
+  return (
+    <div>
+        <MyList />
+    </div>
+
+  )
+}
+
+export default MyContainer
