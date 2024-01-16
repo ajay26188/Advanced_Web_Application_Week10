3935e284e27d3c57bfc2adcd51bb0de8396fb091
ajay26188 <ajaysah52@gmail.com>
2024-01-16 16:10:23 +0200

    last commit

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MyList from './MyList'
 
 const MyContainer = () => {
+    const [items, setItems] = useState([
+        { id: "1", text: "This is an item" },
+        { id: "2", text: "Also this" },
+    ]);
+
+    const [newItems, setNewItems] = useState('');
+
+    const addItem = () => {
+        if (newItems.trim() !== '') {
+            const newItem = {
+                id: String(items.length + 1),
+                text: newItems,
+            };
+            setItems(oldItems => [...oldItems, newItem]);
+            setNewItems('');
+        }
+    }
   return (
     <div>
-        <MyList 
+        <MyList
             header="Really epic last comment"
-            items={[
-                { id: "1", text: "This is an item" },
-                { id: "2", text: "Also this" },
-            ]}
+            items={items}
         />
+        <textarea
+            value={newItems}
+            onChange={(e) => setNewItems(e.target.value)}
+            placeholder="Enter new item"
+        />
+        <button onClick={addItem}>Add Item</button>
+       
+
     </div>
     
   )
