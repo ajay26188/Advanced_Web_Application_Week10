02801328716e9d70439888571cf45759db60895a
ajay26188 <ajaysah52@gmail.com>
2024-01-16 16:50:09 +0200

    last commit

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,8 +3,8 @@ import MyList from './MyList'
 
 const MyContainer = () => {
     const [items, setItems] = useState([
-        { id: "1", text: "This is an item" },
-        { id: "2", text: "Also this" },
+        { id: "1", text: "item1", clicked: false },
+        { id: "2", text: "item2", clicked: false },
     ]);
 
     const [newItems, setNewItems] = useState('');
@@ -14,14 +14,24 @@ const MyContainer = () => {
             const newItem = {
                 id: String(items.length + 1),
                 text: newItems,
+                clicked: false,
             };
             setItems(oldItems => [...oldItems, newItem]);
             setNewItems('');
         }
     }
+    const updateItem = (id) => {
+        setItems(oldItems => 
+            oldItems.map(item =>
+                item.id === id ? {...item, clicked: !item.clicked} : item
+                )
+            )
+    }
+
   return (
     <div>
         <MyList
+            updateItem={updateItem}
             header="Really epic last comment"
             items={items}
         />
--- src/components/MyList.js
+++ src/components/MyList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const MyList = ({header, items}) => {
+const MyList = ({header, items, onClick}) => {
   return (
     <div>
        <h2>{header}</h2> 
        <ol>
             {items.map(item => (
-                <li key={item.id}>{item.text}</li>
+                <li key={item.id} onClick={() => props.updateItem(item.id)} >{item.text}</li>
             ))}
        </ol>
     </div>
@@ -14,3 +14,4 @@ const MyList = ({header, items}) => {
 }
 
 export default MyList
+
